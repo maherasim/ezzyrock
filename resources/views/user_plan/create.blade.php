4b9591c5d1af5ce1a01a3b82351384f0bbb01bc0
maherasim <asimriazasim107@gmail.com>
2026-05-14 09:22:07 +0500

    Free posts

--- resources/views/user_plan/create.blade.php
+++ resources/views/user_plan/create.blade.php
@@ -45,11 +45,6 @@
                             </div>
 
                             <div class="form-group col-md-4">
-                                {{ html()->label('Free Posts Limit', 'free_posts')->class('form-control-label') }}
-                                {{ html()->number('free_posts', $plan->free_posts ?? 0)->placeholder('Free Posts Limit')->class('form-control')->attribute('min', 0)->attribute('step', 1) }}
-                            </div>
-
-                            <div class="form-group col-md-4">
                                 {{ html()->label(trans('messages.plan_limitation') . ' <span class="text-danger">*</span>', 'plan_type')->class('form-control-label') }}
                                 <select class="form-select select2js" id="plan_limitation" name="plan_type">
                                     @foreach($plan_type as $value)
@@ -83,26 +78,11 @@
                         <div>
                             @php
                                 $planValue = $plan->planlimit->plan_limitation ?? [];
-                                $classifiedIsChecked = $planValue['classified']['is_checked'] ?? 'off';
-                                $classifiedLimitValue = $planValue['classified']['limit'] ?? null;
                                 $isChecked = $planValue['featured_classified']['is_checked'] ?? 'off';
                                 $limitValue = $planValue['featured_classified']['limit'] ?? null;
                             @endphp
 
                             <div class="row show-checklist">
-                                <div class="form-group col-md-6">
-                                    <div class="custom-control custom-checkbox custom-control-inline">
-                                        {{ html()->checkbox("plan_limitation[classified][is_checked]", $classifiedIsChecked === 'on', 'on')->class('custom-control-input checklist')->id("enable_classified")->attribute('onClick', "showCheckLimitData('enable_classified')") }}
-                                        <label class="custom-control-label" for="enable_classified">{{ __('messages.plan_limitations', ['keyword' => __('messages.classified')]) }}</label>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 d-none enable_classified" id="show-limit-classified">
-                                    <div class="form-group">
-                                        {{ html()->label(__('messages.limit'), 'classified_limit')->class('form-control-label') }}
-                                        {{ html()->number("plan_limitation[classified][limit]", $classifiedLimitValue)->placeholder(__('messages.plan_limitations', ['keyword' => __('messages.classified')]))->class('form-control')->attribute('min', 0)->attribute('step', 'any') }}
-                                    </div>
-                                </div>
-
                                 <div class="form-group col-md-6">
                                     <div class="custom-control custom-checkbox custom-control-inline">
                                         {{ html()->checkbox("plan_limitation[featured_classified][is_checked]", $isChecked === 'on', 'on')->class('custom-control-input checklist')->id("enable_featured_classified")->attribute('onClick', "showCheckLimitData('enable_featured_classified')") }}
